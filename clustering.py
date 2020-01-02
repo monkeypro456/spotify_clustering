d1e15e7a2146dc34eeeb0304cc2e88e68004b7c0
Prabhat Seth <prabhat.seth@accuity.com>
2020-01-02 18:47:24 +0000

    clustering now creates playlists in spotify. created a getToken function with external yaml.

--- clustering.py
+++ clustering.py
@@ -1,26 +1,13 @@
-import spotipy
-import spotipy.util as util
+'''
+@author: sethp
+@created: 09/10/2019
+'''
+from loadConfig import getToken
+
+from sklearn import preprocessing, cluster, decomposition
 import pandas as pd
 import warnings
-from sklearn import preprocessing, cluster, decomposition
-
-path = 'C:/Users/sethp/Documents/Documents/spotifyresults.csv'
-pd.set_option('display.max_columns',500)
-
-def getToken(scope='playlist-read-private user-read-recently-played user-top-read app-remote-control streaming',
-             username='monkeypro456'):
-    '''
-    :param scope: the scopes that we are intested in
-    :param username: the user that we want data for
-    :return: an authenticated token for connection to a spotify account
-    '''
-    token = util.prompt_for_user_token(
-                username=username,
-                scope=scope,
-                client_id='64e451339e1049acabac90122ddf39ec',
-                client_secret=SECRET_KEY,
-                redirect_uri='http://localhost/')
-    return spotipy.client.Spotify(auth=token)
+import spotipy
 
 def getPlaylistTracks(spotifyConnection=getToken(), playlistName='Prabhat'):
     '''
@@ -28,7 +15,7 @@ def getPlaylistTracks(spotifyConnection=getToken(), playlistName='Prabhat'):
     :param playlistName: the name of the playlist
     :return: a pandas dataframe with information on all the tracks in the playlist
     '''
-    playlist = [i for i in spotifyConnection.user_playlists('monkeypro456')['items'] if i['name'] == playlistName][0]
+    playlist = [i for i in spotifyConnection.user_playlists(spotifyConnection.me()['id'])['items'] if i['name'] == playlistName][0]
     tracks = spotifyConnection.user_playlist(spotifyConnection.me()['id'], playlist['id'])['tracks']
     items = tracks['items']
     while tracks['next']:
@@ -41,7 +28,6 @@ def dictCheck(col):
     :param col: a column of a pandas dataframe
     :return: whether the column contains a dictionary or a list containing dictionaries
     '''
-    #TODO: tried to check if the column is an object but was not doing it correctly :(
     for row in col:
         if isinstance(row, dict):
             return 'dict'
@@ -67,29 +53,14 @@ def dfDictColumn(df, col, suffix):
 
 def nestedDictToDf(df):
     '''
-    a function that loops over itself expanding all dictionaries founc
+    a function that loops over itself expanding all dictionaries found
+
     :requires: pd.DataFrame, pd.DataFrame.join(), pd.add_suffix(), pd.drop(), warnings.warn(), dfDictColumn()
     :param df: a dataframe with columns containing dictionaries or lists of dictionaries
     :return: a dataframe with all dictionary elements expanded out to columns
     '''
-    '''
-    dictCols, listCols = [], []
-    objectCols = df.dtypes[df.dtypes == object].index
-    for col in objectCols: # loop over all columns checking for dictionaries and lists containing dictionaires
-        for row in df[col]:
-
-            if isinstance(row, dict):
-                dictCols.extend([col])
-                break # if it is a dicitionary break
-
-            if isinstance(row, list):
-                if row:
-                    if isinstance(row[0], dict):
-                        listCols.extend([col])
-                        break
-    '''
     objectCols = df.select_dtypes(object).columns
-    series = df[objectCols].apply(dictCheck)
+    series = df[objectCols].apply(dictCheck, axis=0)
     dictCols = list(series.index[series == 'dict'])
     listCols = list(series.index[series == 'list'])
 
@@ -125,17 +96,16 @@ def audioFeatures(df, spotifyConnection=getToken(), suffix='_audiofeat'):
     return df.join(audioFeatures, rsuffix=suffix), audioColumns
 
 def timeSinceAdded(df):
-    df['added_at'] = pd.DatetimeIndex(tracksDf['added_at']).tz_localize(None)
-    df['timeSinceAdded'] = (pd.to_datetime('today') - tracksDf['added_at']).dt.days
+    df['added_at'] = pd.DatetimeIndex(df['added_at']).tz_localize(None)
+    df['timeSinceAdded'] = (pd.to_datetime('today') - df['added_at']).dt.days
     return df
 
-def testing(col):
-    return col.dtype ### WHY DOES THIS NOT WORK. TRY FOR ONE COLUMN AND THEN THE WHOLE DATEFRAME`
-
-tracksDf = getPlaylistTracks()
-tracksDf = nestedDictToDf(tracksDf)
-tracksDf, audioColumns = audioFeatures(df=tracksDf)
-tracksDf = timeSinceAdded(tracksDf)
+def getInputData():
+    tracksDf = getPlaylistTracks()
+    tracksDf = nestedDictToDf(tracksDf)
+    tracksDf, audioColumns = audioFeatures(df=tracksDf)
+    tracksDf = timeSinceAdded(tracksDf)
+    return tracksDf, audioColumns
 
 ## clustering ----------------------------------------------------------------------------------------------------------
 #cap and floor your data to remove outliers (could also just remove outliers)
@@ -146,17 +116,45 @@ def PCA(x):
     x = pca.fit_transform(x)
     return x
 
-def clustering(x, cols, scaler):
+def clustering(x, cols, scaler, method='kmeans'):
+    methods = {'kmeans': cluster.k_means}
     df = x.copy()
     df = pd.DataFrame(scaler.fit_transform(df[cols]), columns=cols)
 
-    kMeans = cluster.k_means(df, n_clusters=5, init='k-means++', precompute_distances=True, random_state=13,
+    method = methods[method]
+    kMeans = method(df, n_clusters=5, init='k-means++', precompute_distances=True, random_state=13,
                              copy_x=True, algorithm='full')
     return kMeans[1]
 
-#minMaxScaler = preprocessing.MinMaxScaler()
-#standardiserScaler = preprocessing.StandardScaler()
-#powerScaler = preprocessing.PowerTransformer(method='yeo-johnson')
-quantileScaler = preprocessing.QuantileTransformer()
-results = clustering(tracksDf, audioColumns, quantileScaler)
-tracksDf['label'] = results
+def createPlaylists(df, audioColumns, scaler, method='kmeans', spotifyConnection=getToken(), stepSize=100):
+    results = clustering(df, audioColumns, scaler)
+    df['label'] = results
+
+    labels = df['label'].unique()
+    today = pd.to_datetime('today')
+
+    for label in labels:
+        name = '{label}_{today}'.format(label=label, today=today)
+        spotifyConnection.user_playlist_create(user= spotifyConnection.me()['id'], name=name, public=False)
+        playlistId = [playlist for playlist in spotifyConnection.user_playlists(spotifyConnection.me()['id'])['items']
+                      if playlist['name'] == name][0]['id']
+
+        tracks = df['uri'][df['label'] == label].to_list()
+        for i in range(0, len(tracks), stepSize):
+            spotifyConnection.user_playlist_add_tracks(user=spotifyConnection.me()['id'], playlist_id=playlistId,
+                                                       tracks=tracks[i:i+stepSize])
+
+    print('This is a playlist created using python clustering. \n' \
+          'Scaler used: {scaler} \n' \
+          'Method used: {method}'.format(scaler=str(type(scaler)), method=method))
+
+
+scaler1 = preprocessing.MinMaxScaler()
+scaler2 = preprocessing.StandardScaler()
+scaler3 = preprocessing.PowerTransformer(method='yeo-johnson')
+scaler4 = preprocessing.QuantileTransformer()
+
+
+if __name__ == '__main__':
+    tracksDf, audioColumns = getInputData()
+    createPlaylists(tracksDf, audioColumns, scaler4)
